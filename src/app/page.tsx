09fcb67e806fed35a733efa52ba5b8d059dbf1c3
Vitali Lavau <v.lavov@imedia.by>
2025-02-25 21:10:20 +0100

    feature/CV-2 -  Главная страница: About

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from '@/app/components/Header/Header';
-import Hero from '@/app/components/Hero/Hero';
+import Hero from '@/app/components/pages/MainPage/Hero/Hero';
+import About from '@/app/components/pages/MainPage/About/About';
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <About />
     </>
   );
 }
