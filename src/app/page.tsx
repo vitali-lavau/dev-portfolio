33f1aa3f7174f9653c691f6f875f9a4554d1590c
Vitali Lavau <v.lavov@imedia.by>
2025-02-26 13:45:52 +0100

    feature/CV-2 -  Главная страница: Skills

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Header from '@/app/components/Header/Header';
 import Hero from '@/app/components/pages/MainPage/Hero/Hero';
 import About from '@/app/components/pages/MainPage/About/About';
+import Skills from '@/app/components/pages/MainPage/Skills/Skills';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <Hero />
       <About />
+      <Skills />
     </>
   );
 }
